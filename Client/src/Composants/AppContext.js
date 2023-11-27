619199ece4020a0ec90599d6978dfc9e48e2d714
Zhuanu <106449996+Zhuanu@users.noreply.github.com>
2023-11-27 22:11:24 +0100

    Delete Client directory

--- Client/src/Composants/AppContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext();
-
-export const TweetContext = createContext();
-
-export const CommentContext = createContext();
